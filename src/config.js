095bfa468dbeb15fcc6ec177eb07b08a41283b2c
dev-back55 <ahabitu@gmail.com>
2022-10-04 13:00:44 -0300

    Feact/rutas y controller nuevos de project y user relacionado project details

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ config();
 
 export const PORT = process.env.PORT || 4001;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/juniordb";
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
