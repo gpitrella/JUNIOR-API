3c0e284b09499845e0303df507528fadc11f6f22
dev-back55 <ahabitu@gmail.com>
2022-10-04 12:57:58 -0300

    Feact/rutas y controller nuevos de project y user relacionado project details

--- src/app.js
+++ src/app.js
@@ -1,43 +1,26 @@
-import express from "express";
-import session from "express-session";
-import methodOverride from "method-override";
-import morgan from "morgan";
-import MongoStore from "connect-mongo";
-import { MONGODB_URI } from "./config.js";
-import routes from './routes/index.routes.js';
-import dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import express from 'express'
+import { CLIENT_URL, PORT } from './config.js';
+import routes from './routes/index.routes.js'
+import morgan from 'morgan';
+const app = express()
 
-dotenv.config()
-const { CLIENT_URL } = process.env;
+app.set('port',PORT)
 
-// Initializations
-const app = express();
+// Middlewares
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 
-// middlewares
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method")); // Sirve para que formulario envie no solo get, put, sino delete o post. 
-app.use( // Sirve para mantener una session del usuario.
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: MONGODB_URI }),
-  })
-);
+app.use("/", routes);
 
 // Enable CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_URL);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, DELETE');
-  next();
+    res.header("Access-Control-Allow-Origin", CLIENT_URL); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, DELETE');
+    next();
 });
-
-// Routes
-app.use("/", routes);
 
 app.use((req, res) => {
   res.render("404");
