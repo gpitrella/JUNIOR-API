0e60f719193af71f89b88859d78a8624eab58e0d
gpitrella <gabriel98_@hotmail.com>
2022-10-26 19:20:41 -0300

    udpdate

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ app.use(morgan('dev'))
 
 // Enable CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", CLIENT_URL); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, DELETE');
